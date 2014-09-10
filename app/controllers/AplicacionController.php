741eaa25c42f92b3afb7b5c245a3b55d05b55eba
omarsancas <omarsanchezcas@gmail.com>
2014-09-10 13:27:27 -0500

    hola que hace

---
+++ app/controllers/AplicacionController.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Omar
+ * Date: 10/09/14
+ * Time: 01:00 PM
+ */
+
+class AplicacionController {
+
+} 
---
+++ app/views/registro/aplicaciones.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Omar
+ * Date: 10/09/14
+ * Time: 01:05 PM
+ */ 
