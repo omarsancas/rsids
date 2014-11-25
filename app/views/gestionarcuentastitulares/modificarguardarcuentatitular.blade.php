ef88e0692a1a54826ad04fd03787635292f35423
omarsancas <omarsanchezcas@gmail.com>
2014-11-25 00:24:51 -0600

    cambio de boton

--- app/views/gestionarcuentastitulares/modificarguardarcuentatitular.blade.php
+++ app/views/gestionarcuentastitulares/modificarguardarcuentatitular.blade.php
@@ -10,7 +10,8 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
-                    Gestionar Proyectos
+                    Gestionar Cuentas Titulares
+
                 </h1>
                 <ol class="breadcrumb">
 
