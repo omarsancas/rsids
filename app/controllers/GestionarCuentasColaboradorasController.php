6c4dd6b86eb06de8d98d74b2d3a714041bd9684a
omarsancas <omarsanchezcas@gmail.com>
2014-11-27 18:50:00 -0600

    Agregar cuentas colaboradoras

---
+++ app/controllers/GestionarCuentasColaboradorasController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Omar
+ * Date: 27/11/14
+ * Time: 06:18 PM
+ */
+
+class GestionarCuentasColaboradorasController extends BaseController {
+
+    public function mostrarAgregarCuentaColaboradora()
+    {
+        return View::make('');
+    }
+} 
